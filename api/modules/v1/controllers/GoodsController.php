3b386c12c40e359bd07df890adc75d427269d766
limin <min.a.li@g-emall.com>
2017-09-04 16:36:22 +0800

    restapi 应用搭建

---
+++ api/modules/v1/controllers/GoodsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+
+class GoodsController extends ActiveController
+{
+    public $modelClass = 'api\models\Goods';
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
